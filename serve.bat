3d0185dc7d315b5d275e1cc2adc4fd902b40bbfe
Shubhanjan Web <69501761+shubhanjanweb@users.noreply.github.com>
2022-02-28 21:57:47 +0530

    added basic spa apps

---
+++ serve.bat
@@ -0,0 +1,24 @@
+echo off
+cd app-shell-root-config 
+start serve
+cd..
+ECHO "app-shell-root-config server started..."
+cd app-shell-root-config 
+start serve
+cd..
+ECHO "app-shell-root-config server started..."
+cd react-components\top-header
+start serve
+cd..
+cd..
+ECHO "react-components\top-header server started..."
+cd react-components\bottom-footer
+start serve
+cd..
+cd..
+ECHO "react-components\bottom-footer server started..."
+cd static-site-host 
+start serve
+cd..
+ECHO "static-site-host server started..."
+
